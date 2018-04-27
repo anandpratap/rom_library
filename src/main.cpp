824d342ba057d9f4d68e52be916109ae747f8433
Anand Pratap Singh <628125+anandpratap@users.noreply.github.com>
2018-04-26 23:46:59 -0400

    working version adeim

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 #include "main.hpp"
 int main(){
+
+	int N = 64; // number of grid points in spatial domain
+	int Nt = 10000; // number of time steps
+	int Tstart = 1; // start time
+	int Tend = 3; // end time
+	int dim = 2; // dimension of (a)DEIM space
+	int w = 1; // window size
+	int ms = 5; // number of additional(!) sampling points
+
+	arma::vec t = arma::linspace<arma::vec>(Tstart, Tend, Nt);
+	arma::vec x = arma::linspace<arma::vec>(0.0, 2.0*arma::datum::pi, N);
+	arma::mat snapshots(N, Nt);
+	for(arma::uword i=0; i<Nt; i++){
+		snapshots.col(i) = test_function(x, t(i));
+	}
+	
 	auto m = Main();
-	m.load_snapshots();
+	m.set_snapshots(snapshots);
+	m.calc_svd();
+	m.calc_deim(dim);
+	for(int i=0; i<Nt; i++){
+		if(i%1000 == 0){
+			m.use_deim(dim, i);
+		}
+	}
+
+	for(int i=0; i<Nt; i++){
+		m.calc_adeim(dim, ms, i);
+		if(i%1000 == 0){
+			m.use_adeim(dim, i);
+		}
+	}
+	//m.load_snapshots();
 	//m.calc_svd();
 	//m.reconstruct();
 	//m.calc_deim();
-	//m.use_deim();
-	m.calc_adeim();
+	
+	//m.calc_adeim();
 }
--- src/main.hpp
+++ src/main.hpp
@@ -23,6 +23,7 @@ arma::vec test_function(arma::vec x, double t){
 	double pi = arma::datum::pi;
 	arma::vec y(x.size());
     y = (sin(t*x) + sin(t*pi/2*x) + sin(t*pi*x))*t;
+	//y = 10*x%x*t*t;
 	return y;
 }
 
@@ -37,28 +38,9 @@ public:
 	arma::vec singular_values;
 
 	arma::Col<arma::uword> deim_p;
-	
-	void load_snapshots(){
-		arma::Mat<int> shape;
-		shape.load("shape.bin", arma::raw_binary);
-		this->snapshots_tmp.load("snapshots.bin", arma::raw_binary);
-		this->snapshots.set_size(shape[1]*shape[2], shape[0]);
 
-		for(int i=0; i<shape[0]; i++){
-			for(int j=0; j<shape[1]; j++){
-				for(int k=0; k<shape[2]; k++){
-					snapshots(j*shape[2] + k, i) = snapshots_tmp(i*shape[2]*shape[1] + j*shape[2] + k);
-				}
-			}
-		}
-		//snapshots = snapshots.submat(arma::span::all, arma::span(0, 20));
-
-		for(int i=0; i< 20; i++){
-			arma::vec snap_t = snapshots.col(i);
-			snap_t.save("snap_t_"+std::to_string(i)+".dat", arma::arma_ascii);
-		}
-
-
+	void set_snapshots(arma::mat isnapshots){
+		snapshots = isnapshots;
 		snap_mean = arma::mean(snapshots, 1);
 		snap_std = arma::stddev(snapshots, 0, 1);
 		print_mat_shape(snap_mean, "Snapshots mean: ");
@@ -73,6 +55,24 @@ public:
 		//snapshots = snapshots.cols(0, 100);
 		print_mat_shape(this->snapshots, "Snapshots: ");
 		println(this->snapshots(0,0));
+	}
+	
+	void load_snapshots(){
+		arma::Mat<int> shape;
+		arma::mat isnapshots;
+		shape.load("shape.bin", arma::raw_binary);
+		this->snapshots_tmp.load("snapshots.bin", arma::raw_binary);
+		isnapshots.set_size(shape[1]*shape[2], shape[0]);
+
+		for(int i=0; i<shape[0]; i++){
+			for(int j=0; j<shape[1]; j++){
+				for(int k=0; k<shape[2]; k++){
+					isnapshots(j*shape[2] + k, i) = snapshots_tmp(i*shape[2]*shape[1] + j*shape[2] + k);
+				}
+			}
+		}
+		//snapshots = snapshots.submat(arma::span::all, arma::span(0, 20));
+		set_snapshots(isnapshots);
 	}
 
 	void calc_svd(){
@@ -93,12 +93,14 @@ public:
 		print("Error in reconstruction: "); println(arma::norm(error, 2)/error.size());
 	}
 
-	void calc_deim(){
-		arma::Col<arma::uword> p(modes_spatial.n_cols);
+	void calc_deim(int dim){
+		arma::Col<arma::uword> p(dim);
 		arma::vec u = arma::abs(modes_spatial.col(0));
+		//u.print();
 		auto imax = u.index_max();
+		println("imax");println(imax);
 		p(0) = imax;
-		for(int i=1; i < modes_spatial.n_cols; i++){
+		for(int i=1; i < dim; i++){
 			println(i);
 			arma::Col<arma::uword> p_sub = p.subvec(0, i-1);
 			arma::mat usub = modes_spatial(arma::span::all, arma::span(0, i-1));
@@ -131,20 +133,23 @@ public:
 	}
 
 
-	void calc_adeim(){
+	void calc_adeim(int dim, int m, int tidx){
 
-		int t = 1500;
+
+
+		
+		int t = tidx;
 		
 		modes_spatial.load("modes_spatial.bin", arma::arma_binary);
 		modes_temporal.load("modes_temporal.bin", arma::arma_binary);
 		singular_values.load("singular_values.bin", arma::arma_binary);
 
 		deim_p.load("p_idx.bin", arma::arma_binary);
-		int n_modes = 100;
+		int n_modes = dim;
 		arma::Col<arma::uword> psub = deim_p.subvec(0, n_modes-1);
 		arma::mat usub = modes_spatial(arma::span::all, arma::span(0, n_modes-1));
-		int m = 10;
-		auto ms = arma::randi<arma::Col<arma::uword>>(m, arma::distr_param(0,usub.n_rows));
+		
+		auto ms = arma::randi<arma::Col<arma::uword>>(m, arma::distr_param(0,usub.n_rows-1));
 		arma::Col<arma::uword> psub_new(psub.size() + m);
 		psub_new.subvec(0, psub.size()-1) = psub;
 		psub_new.subvec(psub.size(), psub.size()+m-1) = ms;
@@ -156,25 +161,50 @@ public:
 		for(int k=0; k<F.size(); k++){
 			F(k) = ut(psub_new(k));
 		}
-		println(psub_new.size());
+		//psub_new.print();
+		//F.print();
+
+// rankTol = 1e-14;
+
+// % compute DEIM coefficients matrix and residual at sampling points
+// size(Uold(S, :))
+// size(F)
+// C = Uold(S, :)\F; % DEIM coefficients
+// size(C)
+// %C
+// res = Uold(S, :)*C - F; % residual at sampling points
+// % reveal rank of C matrix following Lemma 3.4
+// [Q, R, E] = qr(C);
+// %Q, R
+// I = mean(abs(R), 2)/norm(R, 'fro')^2 > rankTol;
+// RTilde = R(I, :);
+// RR = RTilde*RTilde';
+
+
+		
+		//println(psub_new.size());
 		//psub_new.print();
 
 		double ranktol = 1e-14;
 		arma::mat modes_new(usub);
 
 		arma::mat A = usub.rows(psub_new);
-		print_mat_shape(A, "A");
-		arma::vec b = F;
-		arma::vec c = arma::solve(A, b);
-		arma::vec res = A*c - F;
+		//print_mat_shape(A, "A");
+		arma::mat b = F;
+		//print_mat_shape(b, "b");
+		arma::mat c = arma::solve(A, b);
+		arma::mat res = A*c - b;
+		//res.print();
 		arma::mat Q, R;
 		arma::qr(Q, R, c);
-		arma::vec Rabs = arma::abs(R);
-		arma::vec num = arma::mean(Rabs, 1);
+		arma::mat Rabs = arma::abs(R);
+		arma::mat num = arma::mean(Rabs, 1);
 		double den = arma::norm(R, "fro");
-		arma::vec I = num/(den*den);
+		arma::mat I = num/(den*den);
 		arma::uvec I_idx = arma::find(I > ranktol);
-		I_idx.print();
+		
+		//I.print();
+		//I_idx.print();
 		arma::mat R_tilde = R.rows(I_idx);
 		arma::mat RR = R_tilde * R_tilde.t();
 		//arma::mat E = arma::eye(arma::size(Q));
@@ -185,32 +215,126 @@ public:
 		//print_mat_shape(E, "E");
 		//println(res.size());
 		//println(c.size());
+
+// % now use RTilde instead of C to solve eigenvalue problem (Lemma 3.5)
+// % compute update vectors alpha and beta
+// [rMat, eMat] = eig(RTilde*(res*E)'*(res*E)*RTilde', RR);
+// [~, maxI] = max(diag(eMat));
+// normBSquare = rMat(:, maxI)'*RR*rMat(:, maxI);
+// 'Q'
+// size( Q(:, 1:size(RTilde, 1)))
+// 'rmat'
+// size(rMat(:, maxI))
+// beta = Q(:, 1:size(RTilde, 1))*rMat(:, maxI);
+// alpha = -1/normBSquare*res*C'*beta;
+
+// % apply update to basis
+// Unew = Uold;
+// Unew(S, :) = Unew(S, :) + alpha*beta';
+
 		
-		arma::mat E_1 = R_tilde*(res).t()*(res)*R_tilde.t();
+		arma::mat E_1 = R_tilde*res.t()*res*R_tilde.t();
 		arma::cx_vec eigval;
 		arma::cx_mat eigvec;
 		arma::eig_pair(eigval, eigvec, E_1, RR);
-		eigval.print();
-		println(E_1.size());
+		//eigval.print();
+		//println(E_1.size());
 		arma::vec eigval_r = arma::real(eigval);
 		arma::mat eigvec_r = arma::real(eigvec);
-		auto idx_max = eigval_r.index_max();
-		arma::vec col_max = eigvec_r.col(idx_max);
-		arma::mat bnorm_square =  col_max.t()*RR*col_max;
+		arma::uword idx_max = eigval_r.index_max();
+		arma::cx_vec col_max = eigvec.col(idx_max);
+		arma::mat bnorm_square =  arma::real(col_max.t()*RR*col_max);
 		// assert bnorm_square is size 1x1
-		print_mat_shape(bnorm_square, "bnorm");
+		//print_mat_shape(bnorm_square, "bnorm");
 
-		arma::mat beta = Q.cols(0, R_tilde.n_rows-1) * col_max;
-		print_mat_shape(beta, "beta");
+		arma::mat beta = Q.cols(0, R_tilde.n_rows-1) * arma::real(col_max);
+		//print_mat_shape(beta, "beta");
 
 		arma::mat alpha = -1.0/bnorm_square(0,0) * res * c.t() * beta;
-		print_mat_shape(alpha, "alpha");
+		//print_mat_shape(alpha, "alpha");
 
 		arma::mat delta = alpha*beta.t();
+		
 		modes_new.rows(psub_new) = usub.rows(psub_new) + alpha*beta.t();
-		delta.print();
-		println(arma::max(arma::max(delta)));
-		println(arma::min(arma::min(delta)));
+		
+		modes_spatial(arma::span::all, arma::span(0, n_modes-1)) = modes_new;
+
+		modes_spatial.save("modes_spatial.bin", arma::arma_binary);
+
+		//delta.print();
+		//println(arma::max(arma::max(delta)));
+		//println(arma::min(arma::min(delta)));
+
+		arma::mat Unew = modes_new;
+		arma::mat Uold = usub;
+		arma::mat Uno = Unew.t()*Uold;
+		arma::vec Uno_diag = Uno.diag();
+
+		arma::mat Un = Unew.t()*Unew;
+		arma::vec Un_diag = Un.diag();
+		
+		arma::mat Uo = Uold.t()*Uold;
+		arma::vec Uo_diag = Uo.diag();
+
+// [~, minI] = min(abs(diag(Unew'*Uold))./(sqrt(diag(Unew'*Unew)).*sqrt(diag(Uold'*Uold))));
+
+// % remove maxI-th vector from Unew
+// Utmp = Unew;
+// Utmp(:, minI) = [];
+// % remove maxI-th point from P
+// Ptmp = P;
+// Ptmp(minI) = [];
+
+// % find new point following standard DEIM
+// c = Utmp(Ptmp, :)\Unew(Ptmp, minI);
+// r = abs(Unew(:, minI) - Utmp*c); % residual
+// [~, I] = sort(r, 'descend');
+// curI = 1;
+// while(~isempty(find(Ptmp == I(curI), 1))) % check if point is already interp
+//     curI = curI + 1;
+// end
+// P(minI) = I(curI); % add first point that is not already an interp point
+
+		
+		int idx_min = arma::index_min(arma::abs(Uno_diag)/(arma::sqrt(Un_diag)%arma::sqrt(Uo_diag)));
+
+		arma::mat Utmp(Unew);
+		Utmp.shed_col(idx_min);
+		//print_mat_shape(Utmp, "Utmp");
+		arma::uvec ptmp(psub);
+		ptmp.shed_row(idx_min);
+		
+		arma::mat AA = Utmp.rows(ptmp);
+		arma::mat BBt = Unew.rows(ptmp); 
+		arma::mat BB = BBt.col(idx_min);
+		arma::mat cc = arma::solve(AA, BB);
+		//println("c");println(c.size());
+		arma::mat r = Unew.col(idx_min) - Utmp*cc;
+		r = arma::abs(r);
+		arma::uvec ridx = sort_index(r, "descend");
+
+		int curl = 0;
+		
+		while (arma::any(ptmp == ridx(curl))){
+			curl += 1;
+		}
+		//psub(idx_min) = ridx(curl);
+		//psub.print();
+		deim_p.save("p_idx.bin", arma::arma_binary);
+		
+
+		//modes_spatial
+// % find new point following standard DEIM
+// c = Utmp(Ptmp, :)\Unew(Ptmp, minI);
+// r = abs(Unew(:, minI) - Utmp*c); % residual
+// [~, I] = sort(r, 'descend');
+// curI = 1;
+// while(~isempty(find(Ptmp == I(curI), 1))) % check if point is already interp
+//     curI = curI + 1;
+// end
+// P(minI) = I(curI); % add first point that is not already an interp point
+		
+
 	}
 
 
@@ -220,10 +344,27 @@ public:
 		}
 		return x;
 	}
-	
-	void use_deim(){
+
+	void use_adeim(int dim, int t){
 		deim_p.load("p_idx.bin", arma::arma_binary);
-		int n_modes = 100;
+		int n_modes = dim;
+		arma::Col<arma::uword> psub = deim_p.subvec(0, n_modes-1);
+		arma::mat usub = modes_spatial(arma::span::all, arma::span(0, n_modes-1));
+		arma::mat PP = usub * arma::inv(usub.rows(psub));
+		arma::vec snap_t = snapshots.col(t);
+		arma::vec b(n_modes);
+		for(arma::uword k=0; k<n_modes; k++){
+			b(k) = snap_t(psub(k));
+		}
+		arma::vec snap_t_adeim = PP * b;
+		snap_t_adeim = renormalize(snap_t_adeim);
+		snap_t_adeim.save("snap_t_adeim_"+std::to_string(t)+".dat", arma::arma_ascii);
+		
+	}
+	
+	void use_deim(int dim, int t){
+		deim_p.load("p_idx.bin", arma::arma_binary);
+		int n_modes = dim;
 		arma::Col<arma::uword> psub = deim_p.subvec(0, n_modes-1);
 		arma::mat usub = modes_spatial(arma::span::all, arma::span(0, n_modes-1));
 		
@@ -234,9 +375,9 @@ public:
 		arma::mat PP = usub * arma::inv(usub.rows(psub));
 
 		usub.save("spatial_modes.dat", arma::arma_ascii);
-		for(int i=0; i< 20; i++){
-			arma::vec snap_t = snapshots.col(100*i);
-			arma::vec snap_t_pod = snapshots_recon.col(100*i);
+		//		for(int i=1; i < 11; i++){
+			arma::vec snap_t = snapshots.col(t);
+			arma::vec snap_t_pod = snapshots_recon.col(t);
 
 			arma::vec b(n_modes);
 			for(arma::uword k=0; k<n_modes; k++){
@@ -247,15 +388,15 @@ public:
 			snap_t = renormalize(snap_t);
 			snap_t_pod = renormalize(snap_t_pod);
 			snap_t_deim = renormalize(snap_t_deim);
-			snap_t.save("snap_t_"+std::to_string(i)+".dat", arma::arma_ascii);
-			snap_t_pod.save("snap_t_pod_"+std::to_string(i)+".dat", arma::arma_ascii);
-			snap_t_deim.save("snap_t_deim_"+std::to_string(i)+".dat", arma::arma_ascii);
-			psub.save("snap_iblank_"+std::to_string(i)+".dat", arma::arma_ascii);
+			snap_t.save("snap_t_"+std::to_string(t)+".dat", arma::arma_ascii);
+			snap_t_pod.save("snap_t_pod_"+std::to_string(t)+".dat", arma::arma_ascii);
+			snap_t_deim.save("snap_t_deim_"+std::to_string(t)+".dat", arma::arma_ascii);
+			psub.save("snap_iblank_"+std::to_string(t)+".dat", arma::arma_ascii);
 			//arma::vec snap_t_recon = PP * snap_t;
 			auto error_pod_deim = arma::norm(snap_t - snap_t_deim, 2)/snap_t.size();
 			auto error_pod = arma::norm(snap_t - snap_t_pod, 2)/snap_t.size();
 			auto error_deim = arma::norm(snap_t_pod - snap_t_deim, 2)/snap_t.size();
-			println(i);
+			println(t);
 			println("Error POD DEIM = ");
 			println(error_pod_deim);
 			println("Error POD = ");
@@ -263,7 +404,7 @@ public:
 			println("Error DEIM = ");
 			println(error_deim);
 			println("-----------");
-		}
+		
 	};
 };
 
