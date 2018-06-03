1ff114cc6a32c867cbe709000c7deda20b7c7cd6
Anand Pratap Singh <628125+anandpratap@users.noreply.github.com>
2018-06-03 17:32:58 -0400

    Added argument parser to run from commandline and also io arma matrices now than raw_binary

---
+++ src/io/arma.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+header_map = {}
+header_map["ARMA_MAT_BIN_FN008"] = "<f8"
+header_map["ARMA_MAT_BIN_FN004"] = "<f4"
+
+header_map_copy = header_map.copy()
+
+for key, value in header_map_copy.iteritems():
+    header_map[value] = key
+
+def load_mat(filename, verbose=False):
+    f = open(filename, "rb")
+    header = f.readline().replace('\r', '').replace('\n', '')
+    if verbose:
+        print "Reading file: %s"%filename
+    try:
+        dtype = header_map[header]
+        if verbose:
+            print "Header: %s"%header
+    except:
+        raise ValueError("Datatype not understood!")
+    else:
+        sizes = f.readline()
+        n_rows = int(sizes.split()[0])
+        n_cols = int(sizes.split()[1])
+        if verbose:
+            print "Matrix size: %s x %s"%(n_rows, n_cols)
+        x = np.fromfile(f, dtype=dtype)
+        assert x.size == n_rows*n_cols
+        x = np.reshape(x, [n_rows, n_cols], order="F")
+    f.close()
+    return x
+
+def save_mat(x, filename, verbose=False):
+    n, m = x.shape
+    if verbose:
+        print "Writing array of size: %i x %i"%(n, m)
+    try:
+        header = header_map[x.dtype.str]
+        if verbose:
+            print "Header: %s"%header
+    except:
+        raise ValueError("Datatype not understood!")
+
+    f = open(filename, "wb")
+    f.write("%s\n"%header)
+    f.write("%s %s\n"%(n, m))
+    xx = np.reshape(x, [n*m, 1], order="F")
+    xx.tofile(f)
+    f.close()
+
+if __name__ == "__main__":
+    x = load_mat("test.bin", verbose=False)
+    save_mat(x, "test_w.bin", verbose=False)
--- src/io/rom.py
+++ src/io/rom.py
@@ -1,20 +1,23 @@
+import os
 import numpy as np
 import scipy as sp
 import scipy.io as io
+import argparse
 import tecplot
+import arma
 from pylab import *
-def get_filename(fidx):
-    filename = "../../../gems_2/rom_demo/UnsteadyFieldResults/gemsma_cmb_%i.plt"%fidx
+
+def get_filename(directory, fidx):
+    filename = os.path.join(directory, "gemsma_cmb_%i.plt"%fidx)
     return filename
 
 class TecplotImporter(object):
     def __init__(self):
         self.variables_of_interest = ["Static_Pressure","U","V","Temperature","CH4_mf","O2_mf","H2O_mf","CO2_mf"]
-        self.variables_of_interest_deim = ["Eq_%i_residual"%i for i in range(1, len(self.variables_of_interest)+1)]
+        self.variables_of_interest_residual = ["Eq_%i_residual"%i for i in range(1, len(self.variables_of_interest)+1)]
 
         
-    def read_parition_file(self, filename="../../../data_from_flux/dfd_cell.plt"):
-        print filename
+    def read_parition_file(self, filename):
         dataset = tecplot.data.load_tecplot(filename, read_data_option=2)
         zone = dataset.zone(0)
         num_elements = zone.num_elements
@@ -25,7 +28,7 @@ class TecplotImporter(object):
         data_array.tofile("partition.bin")
         print data_array.shape
         
-    def read_data(self, filename, deim):
+    def read_data(self, filename, residual):
         print filename
         dataset = tecplot.data.load_tecplot(filename, read_data_option=2)
         zone = dataset.zone(0)
@@ -35,12 +38,12 @@ class TecplotImporter(object):
         time = zone.solution_time
         variables = list(zone.dataset.variables())
         data = self.read_vars_to_dict(dataset, self.variables_of_interest, num_elements)
-        if deim:
-            data_deim = self.read_vars_to_dict(dataset, self.variables_of_interest_deim, num_elements)
+        if residual:
+            data_residual = self.read_vars_to_dict(dataset, self.variables_of_interest_residual, num_elements)
 
         output = [data]
-        if deim:
-            output.append(data_deim)
+        if residual:
+            output.append(data_residual)
         return output
 
     def read_vars_to_dict(self, dataset, variables_list, num_elements):
@@ -62,19 +65,19 @@ class TecplotImporter(object):
         num_elements = zone.num_elements
         return num_elements
     
-    def read_plts(self, start=1, end=100, step=1, deim=False):
-        ncv = self.query_ncv(get_filename(start))
+    def read_plts(self, start=1, end=100, step=1, residual=False, directory=None):
+        ncv = self.query_ncv(get_filename(directory, start))
         nvar = len(self.variables_of_interest)
         nt = (end + 1 - start)/step
 
         data_array = np.zeros([nt, ncv, nvar], dtype=np.float64)
-        if deim:
-            data_array_deim = np.zeros([nt, ncv, nvar], dtype=np.float64)
+        if residual:
+            data_array_residual = np.zeros([nt, ncv, nvar], dtype=np.float64)
         for fidx, time in enumerate(range(start, end+1, step)):
             print "Loading plts %i of %i"%(fidx+1, end+1-start) 
-            output_data = self.read_data(get_filename(time), deim)
-            if deim:
-                    data_array_deim[fidx, :, :] = output_data[1][:,:]
+            output_data = self.read_data(get_filename(directory, time), residual)
+            if residual:
+                    data_array_residual[fidx, :, :] = output_data[1][:,:]
             else:
                     data_array[fidx, :, :] = output_data[0][:,:]
             # figure()
@@ -83,8 +86,8 @@ class TecplotImporter(object):
             # print "fidx-1", fidx
             # show()
         output = [data_array]
-        if deim:
-            output.append(data_array_deim)
+        if residual:
+            output.append(data_array_residual)
         return output
 
     def save_plts(self):
@@ -92,15 +95,76 @@ class TecplotImporter(object):
         zone = dataset.zone(0)
         
 
+class TecplotExporter(object):
+    def __init__(self):
+        pass
+    
+    def save(self, nmodes, directory=os.getcwd(), residual=False):
+        template_filename = "template.plt"
+        dataset = tecplot.data.load_tecplot(template_filename, read_data_option=2)
+        if not residual:
+            modes = arma.load_mat(os.path.join(directory, "modes_spatial.bin"))
+        else:
+            modes = arma.load_mat(os.path.join(directory, "modes_spatial.bin_residual"))
+        modes_vars_list = ["P_pod_mode", "U_pod_mode", "V_pod_mode", "T_pod_mode",
+                               "Y_CH4_pod_mode", "Y_O2_pod_mode", "Y_H2O_pod_mode", "Y_CO2_pod_mode"]
+        for v in modes_vars_list:
+            dataset.add_variable(v, locations=tecplot.constant.ValueLocation.CellCentered)
+
+        for i in range(nmodes):
+            print "Write mode %i"%i
+            zone = dataset.zone(0)
+            mode = modes[:,i]
+            zone.solution_time = i
+            for idx, v in enumerate(modes_vars_list):
+                zone.values(v)[:] = mode[idx::len(modes_vars_list)]
+            vars_to_write = ["x", "y"]
+            vars_to_write.extend(modes_vars_list)
+            variables = [dataset.variable(v) for v in vars_to_write]
+            if not residual:
+                file_format = "spatial_mode%i.dat"
+            else:
+                file_format = "spatial_mode_residual%i.dat"
+            tecplot.data.save_tecplot_ascii("spatial_mode%i.dat"%(i+1), variables=variables)
+                
 if __name__ == "__main__":
-    t = TecplotImporter()
-    t.read_parition_file()
-    start = 5000
-    end = 11000
-    data_array, data_array_deim = t.read_plts(start=start, end=end, step=1, deim=True)
-    nt, ncv, nvar = data_array.shape
-    sizes = np.array([nt, ncv, nvar], dtype=np.int32)
-    sizes.tofile("snapshots_shape.bin")
-    data_array.tofile("snapshots_solution.bin")
-    data_array_deim.tofile("snapshots_solution.bin"+"_deim", format="%.16f")
-    np.savez("snap_deim.npz", data=data_array_deim)
+    parser = argparse.ArgumentParser(description='Utility for ROM framework')
+    parser.add_argument('--iterations', nargs=2, type=int, help="Start and end (is included) iteration number for parsing and storing snapshots.")
+    parser.add_argument("--process-plts",  action='store_true', help="Read plt files for solution (and residual) snapshots")
+    parser.add_argument("--residual", action="store_true", help="Also process residual snapshots")
+    parser.add_argument("--data-directory", type=str, help="Data directory", default=os.getcwd())
+    
+    parser.add_argument("--save-modes-plts", action="store_true", help="Save modes to tecplot format")
+    parser.add_argument("--nmodes", type=int, help="Number of modes to save")
+    
+    args = parser.parse_args()
+    if args.process_plts:
+        t = TecplotImporter()
+        t.read_parition_file(os.path.join(args.data_directory, "../dfd_cell.plt"))
+        start = args.iterations[0]
+        end = args.iterations[1]
+       
+        if not args.residual:
+            data_array, = t.read_plts(start=start, end=end, step=1, residual=False, directory=args.data_directory)
+            
+            nt, ncv, nvar = data_array.shape
+            sizes = np.array([nt, ncv, nvar], dtype=np.int32)
+            sizes.tofile("snapshots_shape.bin")
+
+            data_array = np.reshape(data_array, [nt, ncv*nvar]).T
+            arma.save_mat(data_array, "snapshots_solution.bin")
+        else:
+            data_array, data_array_residual = t.read_plts(start=start, end=end, step=1, residual=True, directory=args.data_directory)
+
+            nt, ncv, nvar = data_array.shape
+            sizes = np.array([nt, ncv, nvar], dtype=np.int32)
+            sizes.tofile("snapshots_shape.bin")
+            
+            data_array = np.reshape(data_array, [nt, ncv*nvar]).T
+            arma.save_mat(data_array, "snapshots_solution.bin")
+
+            data_array_residual = np.reshape(data_array_residual, [nt, ncv*nvar]).T
+            arma.save_mat(data_array_residual, "snapshots_solution.bin_residual")
+    if args.save_modes_plts:
+        t = TecplotExporter()
+        t.save(args.nmodes, args.data_directory, args.residual)
